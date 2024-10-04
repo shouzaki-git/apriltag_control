587e494fc8e5f0c3a3a483fd950689054103e51a
Sho Uzaki <sho.uzaki@icloud.com>
2024-10-04 12:26:20 +0900

    add new node

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'apriltag_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shouzaki',
+    maintainer_email='sho.uzaki@icloud.com',
+    description='Determine the robot`s position and orientation using Apriltag',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
